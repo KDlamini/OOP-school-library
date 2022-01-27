f6f051e2dda930d0eef5df86c495216f6c605031
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-28 00:41:07 +0200

    Fix all detected offenses.

--- rental.rb
+++ rental.rb
@@ -1,12 +1,11 @@
 class Rental
-    attr_accessor :person, :book, :date
-  
-    def initialize(person, book, date)
-      @person = person
-      person.rentals.push(self)
-      @book = book
-      book.rentals.push(self)
-      @date = date
-    end
+  attr_accessor :person, :book, :date
+
+  def initialize(person, book, date)
+    @person = person
+    person.rentals.push(self)
+    @book = book
+    book.rentals.push(self)
+    @date = date
   end
-  
+end
