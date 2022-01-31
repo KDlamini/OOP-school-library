68f3ce023e552f87b478073118a7204951bcc53b
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-31 14:22:01 +0200

    Bug fixes on student and classroom files.

--- student.rb
+++ student.rb
@@ -1,13 +1,13 @@
 require './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
 
   def initialize(age:, name: 'Unknown', parent_permission: true, classroom: nil)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
-
+  
   def play_hooky
     '¯\(ツ)/¯'
   end
