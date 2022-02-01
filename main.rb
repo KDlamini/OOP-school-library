347aae80b6bd9d56bf915c3719bd2d2dec00575f
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-01 21:58:55 +0200

    Move list_all_rentals_by_id method to listers module

--- main.rb
+++ main.rb
@@ -81,24 +81,6 @@ class App
   attr_reader :users, :books, :rentals
 end
 
-class ListAllRentalById < App
-  def list_all_rental_by_id
-    print 'ID of person: '
-    user_id = gets.chomp
-
-    puts 'Rentals:'
-    rentals.each do |rental|
-      if rental.person.id.to_s == user_id
-        puts "[#{rental.person.class}] Name: #{rental.person.name}
-        | Date: #{rental.date} | Book: \"#{rental.book.title}\" by #{rental.book.author}"
-      end
-    end
-
-    puts "\n"
-    continue?
-  end
-end
-
 def load
   app = App.new
   app.run
