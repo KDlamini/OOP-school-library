cac3eee4a4073547c41194a87cbe724d99e0e8be
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-03 01:08:55 +0200

    Update list_all_books to fetch data from json file.

--- list.rb
+++ list.rb
@@ -11,8 +11,8 @@ module Listers
   end
 
   def list_all_books
-    books.each do |book|
-      puts "Title: #{book.title} | Author: #{book.author}"
+    fetch_saved_data("books").each do |book|
+      puts "Title: #{book["title"]} | Author: #{book["author"]}"
     end
     puts "\n"
     continue?
