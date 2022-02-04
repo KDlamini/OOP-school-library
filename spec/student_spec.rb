f2040cd68d0690451f126c343cf0edb68f60f1fd
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-04 06:34:30 +0200

    Add student_spec.rb test file to the spec folder

---
+++ spec/student_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../student'
+
+describe Student do
+context 'It should add a student' do
+    student = Student.new(classroom: 'new classroom', age: 23, name: 'hamid', parent_permission: true)
+
+    it 'student should be an instance of class Student' do
+        expect(student).to be_an_instance_of(Student)
+    end
+
+    it 'should add a classroom' do
+        expect(student.add_classroom('check room')).to eq('check room')
+    end
+
+    it 'expects method play hooky to return ¯\\(ツ)/¯' do
+        expect(student.play_hooky).to eql("¯\\(ツ)/¯")
+    end
+
+    it 'creates a student with permission' do
+      expect(student.can_use_services?).to eq true
+    end
+
+end
+end
